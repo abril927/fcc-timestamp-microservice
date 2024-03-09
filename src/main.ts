4afb8b1be6210cd4e9eaffb31522c2a0789a7128
Abril <abril927@proton.me>
2024-03-10 00:34:27 +0100

    Fix issues with invalid/nonexistant dates

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,31 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { default as express, Express } from 'express';
+import { default as cors } from 'cors';
 
 let app = express();
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.get('/', (req, res) => {
 	res.send('API: /api/:date');
 });
 
-app.get('/api/:date', (req, res) => {
+app.get('/api/:date?', (req, res) => {
 	const date = req.params.date;
-	let parsed = new Date(date);
-	if (parsed.toString() == 'Invalid Date') {
-		parsed = new Date(parseInt(date));
+	let parsed: Date;
+	if (date != null) {
+		parsed = new Date(date);
+		if (isNaN(parsed.getTime())) {
+			parsed = new Date(parseInt(date));
+			if (isNaN(parsed.getTime())) {
+				return res.json({ error: 'Invalid Date' });
+			}
+		}
+	} else {
+		parsed = new Date();
 	}
-	res.json({ unix: parsed.getTime(), utc: parsed.toUTCString() });
+	
+	return res.json({ unix: parsed.getTime(), utc: parsed.toUTCString() });
 });
 
 app.listen(process.env.PORT || 3000, () => {
